3ec24287866f53a279a3e26bb444c37579430815
Napocore <napocore@gmail.com>
2017-02-17 11:01:08 -0300

    con mejor diseño.
    un desastre el js aun

---
+++ listado.js
@@ -0,0 +1,118 @@
+listadoCelulares = [
+		{
+			"marca": "Motorola",
+			"modelo": "G 4 PLAY",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "8 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB"
+		},
+		{
+			"marca": "Motorola",
+			"modelo": "G 4 PLAY",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "8 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB"
+		},
+		{
+			"id": 1,
+			"marca": "Motorola",
+			"modelo": "G 4 PLAY",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "8 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		},
+		{
+			"id": 2,
+			"marca": "Samsung",
+			"modelo": "GALAXY J5 J510",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5.2,
+			"pantalla_resolucion": "720x1280",
+			"camara_resolucion": "13 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		},
+		{
+			"id": 3,
+			"marca": "Samsung",
+			"modelo": "GALAXY J7 J710",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5.5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "13 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		},
+		{
+			"id": 4,
+			"marca": "Huawei",
+			"modelo": "Y360D",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 4,
+			"pantalla_resolucion": "800x480",
+			"camara_resolucion": "5 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "2 Mpx",
+			"memoria_interna": "4 GB",
+			"flash": true
+		},
+		{
+			"id": 5,
+			"marca": "Lenovo",
+			"modelo": "VIBE K5",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "800x480",
+			"camara_resolucion": "13 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		}
+	];
+
+
+function agregarElementos() {
+	var p = document.createElement("p");
+	p.id = "nuevo";
+	var t = document.createTextNode("[0].marca");
+	p.appendChild(t);
+	var div = document.createTextNode("texto");
+	p.appendChild(t)
+
+}
+for (var cont=0;cont <listadoCelulares.lenght;cont++){
+	var p = document.createElement ("li");
+	//opcion 1 generar el texto en una variable y ponerla en el nodo de texto 
+	var texto = "marca"+listadoCelulares [cont]marca;
+	var valor = document.createTextNode (texto)
+	p.appendChild (valor);
+	var div = document.getElementById("nuevosElementos");
+	p.appendChild (p)
+//opcion 2 generar dos nodos de tex8to on la caractersitca y el valor por separado
+var opcion2: document.createElement"li";
+var caractersitca =document.createTextNode("marca:");
+opcion2.appendChild(valorCaracteristica);
+var valorCaracteristica =document.createTextNode (listadoCelulares[cont].marca);
+opcion2.appendChild (valorCaracteristica;)
+var div :document.getElementById("nuevosElementos");
+div.appendChild (opcion2);
+
+}
