58089682952561c8dc52272fffe8be03b77c4621
Napocore <napocore@gmail.com>
2017-02-24 11:10:02 -0300

    Add files via upload

--- src/main/webapp/js/listado.js
+++ src/main/webapp/js/listado.js
@@ -12,26 +12,26 @@ listadoCelulares = [
 		},
 		{
 			"marca": "Motorola",
-			"modelo": "G 4 PLAY",
+			"modelo": "X",
 			"sistema_operativo": "Android",
-			"pantalla_pulgadas": 5,
-			"pantalla_resolucion": "1280x720",
+			"pantalla_pulgadas": 6,
+			"pantalla_resolucion": "1580x1120",
 			"camara_resolucion": "8 Mpx",
-			"camara_frontal": true,
+			"camara_frontal": false,
 			"camara_frontal_res": "5 Mpx",
-			"memoria_interna": "16 GB"
+			"memoria_interna": "32 GB"
 		},
 		{
 			"id": 1,
 			"marca": "Motorola",
 			"modelo": "G 4 PLAY",
 			"sistema_operativo": "Android",
-			"pantalla_pulgadas": 5,
+			"pantalla_pulgadas": 5.1,
 			"pantalla_resolucion": "1280x720",
 			"camara_resolucion": "8 Mpx",
 			"camara_frontal": true,
 			"camara_frontal_res": "5 Mpx",
-			"memoria_interna": "16 GB",
+			"memoria_interna": "24 GB",
 			"flash": true
 		},
 		{
@@ -40,11 +40,11 @@ listadoCelulares = [
 			"modelo": "GALAXY J5 J510",
 			"sistema_operativo": "Android",
 			"pantalla_pulgadas": 5.2,
-			"pantalla_resolucion": "720x1280",
-			"camara_resolucion": "13 Mpx",
+			"pantalla_resolucion": "1720x1280",
+			"camara_resolucion": "14 Mpx",
 			"camara_frontal": true,
-			"camara_frontal_res": "5 Mpx",
-			"memoria_interna": "16 GB",
+			"camara_frontal_res": "6 Mpx",
+			"memoria_interna": "32 GB",
 			"flash": true
 		},
 		{
@@ -55,7 +55,7 @@ listadoCelulares = [
 			"pantalla_pulgadas": 5.5,
 			"pantalla_resolucion": "1280x720",
 			"camara_resolucion": "13 Mpx",
-			"camara_frontal": true,
+			"camara_frontal": false,
 			"camara_frontal_res": "5 Mpx",
 			"memoria_interna": "16 GB",
 			"flash": true
@@ -81,7 +81,7 @@ listadoCelulares = [
 			"pantalla_pulgadas": 5,
 			"pantalla_resolucion": "800x480",
 			"camara_resolucion": "13 Mpx",
-			"camara_frontal": true,
+			"camara_frontal": false,
 			"camara_frontal_res": "5 Mpx",
 			"memoria_interna": "16 GB",
 			"flash": true
@@ -91,9 +91,18 @@ listadoCelulares = [
 
 function agregarElementos() {
 	var p = document.createElement("p");
-	p.id = "nuevo";
-	var t = document.createTextNode("Esto lo insertamos dinámicamente");
+	p.id = "nuevosElementos";
+	var t = document.createTextNode("LISTO ! agregaste un elemento !! ");
 	p.appendChild(t);
-	var div = document.getElementById("nuevosElementos");
+	var div = document.getElementById("nuevo");
 	div.appendChild(p);
 }
+var objeto= lista 
+
+function <button onclick="alert()">alerta</button>
+
+<script>
+function alert() {
+    alert("soy una alerta!");
+}
+</script>
